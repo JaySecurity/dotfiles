43c31566dbbb1e9952daf69d74910650546b7d30
Jason Nicholls <jasonnicholls1981@gmail.com>
2025-01-13 14:27:11 -0500

    Update i3 volume control for pipewire

--- scripts/.config/scripts/browser.sh
+++ scripts/.config/scripts/browser.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 while [ -z "$select" ]; do
-  select=$(echo -e 'Brave\nDev-Brave\nFirefox\nQute\nChromium' | dmenu -i -p "Which browser would you like to open?" -fn 'DejaVuSansM Nerd Font Mono-14')
+  select=$(echo -e 'Brave\nDev-Brave\nFirefox\nQute\nChromium' | dmenu -i -p "Which browser would you like to open?" -fn 'DejaVuSansM Nerd Font Mono-12')
   [ -z "$select" ] && exit 0
 done
 case $select in
